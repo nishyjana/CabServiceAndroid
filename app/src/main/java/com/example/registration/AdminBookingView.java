da79d24fc2e0eac49dba374dc346cde3658164a6
nishy <nishyjana6435@gmail.com>
2021-01-28 17:39:16 +0530

    latest update

--- app/src/main/java/com/example/registration/AdminBookingView.java
+++ app/src/main/java/com/example/registration/AdminBookingView.java
@@ -1,3 +1,4 @@
+
 package com.example.registration;
 
 import android.os.Bundle;
@@ -18,8 +19,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -56,18 +61,45 @@ public class AdminBookingView extends AppCompatActivity {
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull AdminBookingView.bookingViewholder holder, int position, @NonNull final cabBook model) {
+            protected void onBindViewHolder(@NonNull final AdminBookingView.bookingViewholder holder, int position, @NonNull final cabBook model) {
 
                 holder.booking_name.setText(model.getName());
                 holder.booking_location.setText(model.getPickup());
+
+
+
+                holder.spinner.setAdapter(holder.adapter);;
                 holder.driverone.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Toast.makeText(AdminBookingView.this, "This is" +model.getName(), Toast.LENGTH_SHORT).show();
+                        String state = holder.spinner.getSelectedItem().toString();
+
+                        Toast.makeText(AdminBookingView.this, "This is" +state, Toast.LENGTH_SHORT).show();
+
+
+                        DocumentSnapshot snapshot = getSnapshots().getSnapshot(holder.getAdapterPosition());
+                        String DocID = snapshot.getId();
+                        DocumentReference docRef = firebaseFirestore.collection("Bookings").document(DocID);
+                        docRef
+                                .update("driver", state)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(AdminBookingView.this, " This is sucess  ", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+
+                                    }
+                                });
+
+
 
                     }
                 });
-                holder.spinner.setAdapter(holder.adapter);;
+
 
                 //Toast.makeText(AdminBookingList.this, "This is" +model.getName(), Toast.LENGTH_SHORT).show();
 
@@ -95,12 +127,17 @@ public class AdminBookingView extends AppCompatActivity {
             booking_location= itemView.findViewById(R.id.bookinglocation);
             driverone =itemView.findViewById(R.id.driveronedets);
             spinner= itemView.findViewById(R.id.spinner);
+
+
             FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
             CollectionReference subjectsRef = rootRef.collection("Users");
+            Query querys = subjectsRef.whereEqualTo("driver",true).whereEqualTo("status","Active");
+
 
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-            subjectsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            querys.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
                 @Override
                 public void onComplete(@NonNull Task<QuerySnapshot> task) {
                     if (task.isSuccessful()) {
@@ -114,7 +151,7 @@ public class AdminBookingView extends AppCompatActivity {
             });
 
 
-             }
+        }
     }
 
     @Override
