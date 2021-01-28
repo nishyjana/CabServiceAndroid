c1bf72501ca73e9984ba163ef93fffc7bb1b9031
nishy <nishyjana6435@gmail.com>
2021-01-28 13:59:50 +0530

    list view done

--- app/src/main/java/com/example/registration/AdminBookingView.java
+++ app/src/main/java/com/example/registration/AdminBookingView.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,8 +17,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminBookingView extends AppCompatActivity {
 
@@ -47,6 +57,7 @@ public class AdminBookingView extends AppCompatActivity {
 
             @Override
             protected void onBindViewHolder(@NonNull AdminBookingView.bookingViewholder holder, int position, @NonNull final cabBook model) {
+
                 holder.booking_name.setText(model.getName());
                 holder.booking_location.setText(model.getPickup());
                 holder.driverone.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +67,8 @@ public class AdminBookingView extends AppCompatActivity {
 
                     }
                 });
+                holder.spinner.setAdapter(holder.adapter);;
+
                 //Toast.makeText(AdminBookingList.this, "This is" +model.getName(), Toast.LENGTH_SHORT).show();
 
             }
@@ -66,16 +79,42 @@ public class AdminBookingView extends AppCompatActivity {
 
     }
 
+
     private class bookingViewholder extends RecyclerView.ViewHolder {
         private TextView booking_name, booking_location;
         private Button driverone;
+        List<String> subjects = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, subjects);
+        Spinner spinner;
+
+
 
         public bookingViewholder(@NonNull View itemView) {
             super(itemView);
             booking_name = itemView.findViewById(R.id.bookingname);
             booking_location= itemView.findViewById(R.id.bookinglocation);
             driverone =itemView.findViewById(R.id.driveronedets);
-        }
+            spinner= itemView.findViewById(R.id.spinner);
+            FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+            CollectionReference subjectsRef = rootRef.collection("Users");
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            subjectsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String subject = document.getString("name");
+                            subjects.add(subject);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            });
+
+
+             }
     }
 
     @Override
